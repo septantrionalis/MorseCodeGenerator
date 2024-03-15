c9310967360f2ed79ce99a78b90f903613dc8df3
Ron <rkinney@tdod.org>
2024-03-15 13:37:14 -0600

    fix build stuff

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mvn clean compile assembly:single
-mv ./target/mcgenerator-jar-with-dependencies.jar ./app
-mv ./target/mcgenerator-jar-with-dependencies.jar ./dist
+cp ./target/mcgenerator-jar-with-dependencies.jar ./app
+cp ./target/mcgenerator-jar-with-dependencies.jar ./dist
 
