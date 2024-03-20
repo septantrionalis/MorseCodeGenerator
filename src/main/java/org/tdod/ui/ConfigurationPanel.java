e48c0789dc8e6b9669e1cde32913113da2c550bd
Ron Kinney <rokinney@ghx.com>
2024-03-20 17:10:36 -0600

    gui dev

--- src/main/java/org/tdod/ui/ConfigurationPanel.java
+++ src/main/java/org/tdod/ui/ConfigurationPanel.java
@@ -20,8 +20,11 @@ public class ConfigurationPanel extends JPanel {
     private JTextField startDelayTextField;
     private JTextField audioFrequencyTextField;
 
+    private JPanel outerPanel = new JPanel();
+
     public ConfigurationPanel() {
-        this.setLayout(new GridBagLayout());
+        this.setLayout(new BorderLayout());
+        outerPanel.setLayout(new GridBagLayout());
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.anchor = GridBagConstraints.NORTHWEST;
@@ -30,60 +33,69 @@ public class ConfigurationPanel extends JPanel {
         int row = 0;
         createLabel(gbc, "OpenAI API Key:", 0, row);
         apiKeyTextField = createTextField(gbc, 1, row, 16, false);
-        this.add(apiKeyTextField, gbc);
+        outerPanel.add(apiKeyTextField, gbc);
         apiKeyTextField.setText(Configuration.getApiKey());
 
         row++;
         createLabel(gbc, "OpenAI Text Prompt:", 0, row);
         apiPromptTextField = createTextField(gbc, 1, row, 16, false);
-        this.add(apiPromptTextField, gbc);
+        outerPanel.add(apiPromptTextField, gbc);
         apiPromptTextField.setText(Configuration.getTextPrompt());
 
         row++;
         createLabel(gbc, "OpenAI URL:", 0, row);
         apiUrlTextField = createTextField(gbc, 1, row, 16, false);
-        this.add(apiUrlTextField, gbc);
+        outerPanel.add(apiUrlTextField, gbc);
 
         row++;
         createLabel(gbc, "OpenAI Model:", 0, row);
         openAiTextField = createTextField(gbc, 1, row, 16, false);
-        this.add(openAiTextField, gbc);
+        outerPanel.add(openAiTextField, gbc);
 
         row++;
         createLabel(gbc, "WPM:", 0, row);
         wpmComboBox = new JComboBox(WpmEnum.getDisplayNames());
         gbc.gridx = 1;
         gbc.gridy = row;
-        this.add(wpmComboBox, gbc);
+        outerPanel.add(wpmComboBox, gbc);
 
         row++;
         createLabel(gbc, "Text Source:", 0, row);
         textSourceComboBox = new JComboBox(TextSourceEnum.getDisplayNames());
         gbc.gridx = 1;
         gbc.gridy = row;
-        this.add(textSourceComboBox, gbc);
+        outerPanel.add(textSourceComboBox, gbc);
 
         row++;
         createLabel(gbc, "Text Filename:", 0, row);
         filenameTextField = createTextField(gbc, 1, row, 16, false);
-        this.add(filenameTextField, gbc);
+        outerPanel.add(filenameTextField, gbc);
 
         row++;
         createLabel(gbc, "Start Delay:", 0, row);
         startDelayTextField = createTextField(gbc, 1, row, 3, false);
-        this.add(startDelayTextField, gbc);
+        outerPanel.add(startDelayTextField, gbc);
 
         row++;
         createLabel(gbc, "Audio Frequency:", 0, row);
         audioFrequencyTextField = createTextField(gbc, 1, row, 4, true);
-        this.add(audioFrequencyTextField, gbc);
+        outerPanel.add(audioFrequencyTextField, gbc);
+        this.add(outerPanel, BorderLayout.WEST);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+        JButton button = new JButton("Save");
+        button.setSize(50, 50);
+        buttonPanel.add(button);
+        this.add(buttonPanel, BorderLayout.SOUTH);
+
     }
 
     private void createLabel(GridBagConstraints gbc, String label, int x, int y) {
         JLabel jlabel = new JLabel(label, SwingConstants.LEFT);
         gbc.gridx = x;
         gbc.gridy = y;
-        this.add(jlabel, gbc);
+        outerPanel.add(jlabel, gbc);
     }
 
     private JTextField createTextField(GridBagConstraints gbc, int x, int y, int size, boolean isLast) {
