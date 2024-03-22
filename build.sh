df66aafae76ac9b349feb4867f0b2ba36bc3c913
Ron <rkinney@tdod.org>
2024-03-21 22:15:24 -0600

    fixes

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+echo Reseting the config file...
+echo api.ai.key=TODO > config.properties
+echo api.ai.textprompt=a lengthy weather report >> config.properties
+echo api.ai.url=https://api.openai.com/v1/chat/completions >> config.properties
+echo api.ai.model=gpt-3.5-turbo >> config.properties
+echo app.wpm=23 >> config.properties
+echo app.textsource=file >> config.properties
+echo app.startdelay=3 >> config.properties
+echo audio.frequency=600 >> config.properties
+echo app.filename=space_weather1.txt >> config.properties
+
 mvn clean compile assembly:single
 cp ./target/mcgenerator-jar-with-dependencies.jar ./app
 cp ./target/mcgenerator-jar-with-dependencies.jar ./dist
